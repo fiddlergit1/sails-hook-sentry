113ad0b741e7b321ba98a159e5d480b654d86d29
Jussi Kaijalainen <jussi(at)wingmen.fi>
2022-09-03 10:56:23 +0800

    Cleanup + simplify passing config

--- hook.test.js
+++ hook.test.js
@@ -1,37 +1,40 @@
-const Sails = require('sails')
+const Sails = require("sails");
 
-describe('Asserts that sentry reworked hook works well', function () {
+describe("Asserts that sentry reworked hook works well", function () {
   let sails;
 
   before(function (done) {
-    this.timeout(0)
+    this.timeout(0);
 
-    Sails.lift({
-      hooks: {
-        "sentry": require('./index'),
-        "grunt": false
+    Sails.lift(
+      {
+        hooks: {
+          sentry: require("./index"),
+          grunt: false,
+        },
+        log: {
+          level: "error",
+        },
       },
-      log: {
-        level: "error"
+      function (err, _sails) {
+        if (err) {
+          return done(err);
+        }
+        sails = _sails;
+        return done();
       }
-    }, function (err, _sails) {
-      if (err) {
-        return done(err)
-      }
-      sails = _sails;
-      return done()
-    })
-  })
+    );
+  });
 
   after(function (done) {
     if (sails) {
-      return sails.lower(done)
+      return sails.lower(done);
     }
-    return done()
-  })
+    return done();
+  });
 
   it("Should assert sails doesnt crash", function () {
-    console.log(sails.hooks.sentry)
-    return true
-  })
-})
+    console.log(sails.hooks.sentry);
+    return true;
+  });
+});
--- index.js
+++ index.js
@@ -7,32 +7,22 @@ module.exports = function Sentry(sails) {
      * @return {Function} cb Callback for when we're done initializing
      */
     initialize: function (cb) {
-      var settings = sails.config[this.configKey];
+      const settings = sails.config[this.configKey];
       if (!settings.active) {
         sails.log.info("Sentry hook was set to inactive in config.");
         return cb();
       }
+      delete settings.active;
 
       if (!settings.dsn) {
         sails.log.error("DSN for Sentry is required.");
         return cb();
       }
 
-      const config = {
-        dsn: settings.dsn,
-      };
-
-      if (settings?.options?.release) {
-        config.release = settings.options.release;
-      }
-
-      if (settings?.options?.environment) {
-        config.environment = settings.options.environment;
-      }
-
-      Sentry.init(config);
+      Sentry.init(settings);
       sails.log.info(`Sentry initialized with DSN: `, settings.dsn);
 
+      // hook sentry to the sails object
       sails.sentry = Sentry;
 
       // handles Bluebird's promises unhandled rejections
@@ -41,7 +31,6 @@ module.exports = function Sentry(sails) {
         Sentry.captureException(reason);
       });
 
-      // We're done initializing.
       return cb();
     },
     sentry: Sentry,
